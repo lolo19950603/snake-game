7f603f9b77f7757ebc2ec53f293e1c3ff5b753aa
loren950603 <loren840603@gmail.com>
2022-07-09 12:06:17 -0400

    adding a score board

--- SnakeGame/script.js
+++ SnakeGame/script.js
@@ -1,71 +1,66 @@
 const snake = document.querySelector("#snake");
 
-let current_position = { x: 0, y: 0 };
-let current_direction = "right";
+let currentPosition = { x: 0, y: 0 };
+let currentDirection = "right";
 
-let brower_dimension = { width: window.innerWidth, height: window.innerHeight };
+let browerDimensions = { width: 670, height: 670 };
 
 
 
-function updateGameDimension() {
-  brower_dimension.width = window.innerWidth;
-  brower_dimension.height = window.innerHeight;
-}
+// function updateGameDimension() {
+//   browerDimensions.width = window.innerWidth;
+//   browerDimensions.height = window.innerHeight;
+// }
 
 setInterval(() => {
-  if (current_direction === "right") {
-    if (current_position.x === brower_dimension.width) {
-      current_position.x = 0;
+  if (currentDirection === "right") {
+    if (currentPosition.x === browerDimensions.width) {
+      currentPosition.x = 0;
     } else {
-      current_position.x++;
+      currentPosition.x++;
     }
     snake.style =
-      "top: " + current_position.y.toString() + "px; " +
-      "left: " + current_position.x.toString() + "px;";
-  } else if (current_direction === "left") {
-    if (current_position.x === 0) {
-      current_position.x = brower_dimension.width;
+      "top: " + currentPosition.y.toString() + "px; " +
+      "left: " + currentPosition.x.toString() + "px;";
+  } else if (currentDirection === "left") {
+    if (currentPosition.x === 0) {
+      currentPosition.x = browerDimensions.width;
     } else {
-      current_position.x--;
+      currentPosition.x--;
     }
     snake.style =
-      "top: " + current_position.y.toString() + "px; " +
-      "left: " + current_position.x.toString() + "px;";
-  } else if (current_direction === "down") {
-    if (current_position.y === brower_dimension.height) {
-      current_position.y = 0;
+      "top: " + currentPosition.y.toString() + "px; " +
+      "left: " + currentPosition.x.toString() + "px;";
+  } else if (currentDirection === "down") {
+    if (currentPosition.y === browerDimensions.height) {
+      currentPosition.y = 0;
     } else {
-      current_position.y++;
+      currentPosition.y++;
     }
     snake.style =
-      "top: " + current_position.y.toString() + "px; " +
-      "left: " + current_position.x.toString() + "px;";
-  } else if (current_direction === "up") {
-    if (current_position.y === 0) {
-      current_position.y = brower_dimension.height;
+      "top: " + currentPosition.y.toString() + "px; " +
+      "left: " + currentPosition.x.toString() + "px;";
+  } else if (currentDirection === "up") {
+    if (currentPosition.y === 0) {
+      currentPosition.y = browerDimensions.height;
     } else {
-      current_position.y--;
+      currentPosition.y--;
     }
     snake.style =
-      "top: " + current_position.y.toString() + "px; " +
-      "left: " + current_position.x.toString() + "px;";
+      "top: " + currentPosition.y.toString() + "px; " +
+      "left: " + currentPosition.x.toString() + "px;";
   }
-  updateGameDimension()
 }, 5);
 
 function handleMove(e) {
   if (e.keyCode === 38) {
-    console.log("up arrow pressed");
-    current_direction = "up";
+    currentDirection = "up";
   } else if (e.keyCode === 40) {
-    console.log("down arrow pressed");
-    current_direction = "down";
+    currentDirection = "down";
   } else if (e.keyCode === 37) {
-    console.log("left arrow pressed");
-    current_direction = "left";
+    currentDirection = "left";
   } else if (e.keyCode === 39) {
-    console.log("right arrow pressed");
-    current_direction = "right";
+    currentDirection = "right";
   }
 }
 
